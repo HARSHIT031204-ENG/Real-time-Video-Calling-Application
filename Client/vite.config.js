c3c6ad418f95632d9c15f6cd618e6536850e9b76
Harshit Garg <gargh2120@gmail.com>
2025-11-20 00:33:44 +0530

    fetures define

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    https: false,
+    allowedHosts: [
+      "localhost",
+      "*.ngrok-free.app",
+      "*.ngrok-free.dev",
+      "gilma-sleetiest-lyn.ngrok-free.dev"
+    ],
+    cors: true
+  }
 })
