24175c6274b8f055309cdad9eb7af053f2c57c00
Harshit Garg <gargh2120@gmail.com>
2025-11-19 10:40:02 +0530

    fix error

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LobbyScreen from "./screens/Lobby";
+import Room from "./screens/Room";
+
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<LobbyScreen/>} />
+                <Route path="/room/:roomID" element={<Room/>} />
             </Routes>
         </>
     );
