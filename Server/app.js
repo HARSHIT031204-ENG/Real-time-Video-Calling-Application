6ebe2485104d2b928ac98728700210c23e7b6587
Harshit Garg <gargh2120@gmail.com>
2025-11-19 10:42:31 +0530

    fetures define

--- Server/app.js
+++ Server/app.js
@@ -1,26 +1,35 @@
 import { Server } from "socket.io";
 
-
 const io = new Server(8000, {
-    cors : {
-        origin : "http://localhost:5173",
-        methods : ["POST", "GET", "PUT"],
-        credentials : true
-    }
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["POST", "GET", "PUT"],
+        credentials: true,
+    },
 });
 
 const emailToSocketIdMap = new Map();
 const socketidToEmailMap = new Map();
 
-
 io.on("connection", (socket) => {
     console.log(`Welcome user and user ID : ${socket.id}`);
-    socket.on("room:join", (data) => {console.log(data);
-        const {email, room} = data 
+    socket.on("room:join", (data) => {
+        console.log(data);
+        const { email, room } = data;
         emailToSocketIdMap.set(email, socket.id);
         socketidToEmailMap.set(socket.id, email);
-        io.to(room).emit("user:joined", { email, id: socket.id });
-        
-    })
-})
+        io.to(room).emit("user-joined", { email, id: socket.id });
+        socket.join(room);
+        io.to(socket.id).emit("room:join", data);
+    });
 
+    socket.on("user:call", ({ to, offer }) => {
+        io.to(to).emit("incomming:call", { from: socket.id, offer });
+    });
+
+    socket.on("call:accepted", ({ to, ans }) => {
+        io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
+
+    
+});
