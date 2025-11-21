4e80ee1f6ad9ad09b106c80f667d4322cdbae45a
Harshit Garg <gargh2120@gmail.com>
2025-11-21 22:08:47 +0530

    updated socket provider

--- Server/app.js
+++ Server/app.js
@@ -1,47 +1,49 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 
-const io = new Server(8000, {
-    cors: {
-        origin: "*",
-        methods: ["POST", "GET", "PUT"],
-        credentials: true,
-    },
-});
+const app = express();
+app.use(cors());
 
-const emailToSocketIdMap = new Map();
-const socketidToEmailMap = new Map();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-    console.log(`Welcome user and user ID : ${socket.id}`);
-    socket.on("room:join", (data) => {
-        console.log(data);
-        const { email, room } = data;
-        emailToSocketIdMap.set(email, socket.id);
-        socketidToEmailMap.set(socket.id, email);
-        io.to(room).emit("user-joined", { email, id: socket.id });
-        socket.join(room);
-        io.to(socket.id).emit("room:join", data);
-    });
+  console.log("User connected", socket.id);
 
-    socket.on("user:call", ({ to, offer }) => {
-        io.to(to).emit("incomming:call", { from: socket.id, offer });
-    });
+  socket.on("room:join", ({ email, room }) => {
+    socket.join(room);
+    socket.to(room).emit("user-joined", { email, id: socket.id });
+    io.to(socket.id).emit("room:join", { email, room });
+  });
 
-    socket.on("call:accepted", ({ to, ans }) => {
-        io.to(to).emit("call:accepted", { from: socket.id, ans });
-    });
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
 
-    socket.on("peer:nego:done", ({ to, ans }) => {
-        console.log("peer:nego:done", ans);
-        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-    });
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
 
-    socket.on("peer:nego:needed", ({ to, offer }) => {
-        console.log("peer:nego:needed", offer);
-        io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-    });
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
 
-    socket.on("call:end", ({ to }) => {
-        io.to(to).emit("call:ended");
-    });
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+
+  socket.on("call:end", ({ to }) => {
+    io.to(to).emit("call:ended");
+  });
 });
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
